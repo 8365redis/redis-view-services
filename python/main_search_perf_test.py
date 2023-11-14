1c816e0a49c126e1b478db6a7b66970b7a3f38c0
Burak Karamik <burak.karamik@toptal.com>
2023-11-14 15:52:05 +0300

    New perf test for view

---
+++ python/main_search_perf_test.py
@@ -0,0 +1,36 @@
+from manage_redis import kill_redis, connect_redis_with_start, connect_redis
+from manage_redis_data import flush_db, create_index, add_json_data, TEST_INDEX_NAME
+import pytest
+import time
+import hashlib
+
+
+@pytest.fixture(autouse=True)
+def before_and_after_test():
+    print("Start")
+    yield
+    kill_redis()
+    print("End")
+
+def test_add_perf_data_and_search():
+    producer = connect_redis_with_start()
+    flush_db(producer) # clean all db first
+    create_index(producer)
+
+    start_time = time.time()
+    add_json_data(producer, 100000, 10)
+    end_time = time.time()
+    json_set_latency = (int)(end_time - start_time) * 1000  # time.time returns ns
+    print("Module enabled latency = " + str(json_set_latency) + "ms")
+
+    client_count = 100
+    query_value = "data1"
+    for i in range(client_count):
+        client = connect_redis()
+        client.execute_command("VIEW.REGISTER " + "client-" + str(i))
+        client.execute_command("VIEW.MAIN.SEARCH test_index @base\\.data1:{" + query_value  + "}")
+    
+    time.sleep(15)
+
+    res = producer.execute_command("INFO latencystats")
+    print(str(res))
